8c4aa4f0d6242cba64e47876c35d7b87ed27ac28
Christian Bagaya <chrisbag94@gmail.com>
2021-04-05 00:05:51 -0700

    initial commit

---
+++ virtual_assistant.py
@@ -0,0 +1,69 @@
+import speech_recognition as sr
+import pyttsx3
+import pywhatkit
+import datetime
+import wikipedia
+listener = sr.Recognizer()
+name_of_assistant = "Safi"
+
+eng_speaker = pyttsx3.init()
+#Set female voice
+eng_speaker.setProperty('voice', eng_speaker.getProperty('voices')[1].id)
+
+def speak(text):
+    eng_speaker.say(text)
+    eng_speaker.runAndWait()
+
+
+def take_input_command():
+    voice_command=""
+    try:
+        with sr.Microphone() as src:
+            speak("Hey there, I am"+ name_of_assistant+", How can I help you?")
+            voice = listener.listen(src)
+            
+
+            voice_command = listener.recognize_google(voice)
+            voice_command = voice_command.lower()
+
+            if name_of_assistant in voice_command:
+                voice_command = voice_command.replace(name_of_assistant, "")
+            
+                # print("Your command: "+voice_command)
+
+    except:
+        pass
+    
+
+    return voice_command 
+
+def main():
+    command = take_input_command()
+    print(command)
+
+
+    #Playing a video on youtube
+    if 'play' in command:
+        command = command.replace("play", "")
+
+        speak("playing"+command+" youtube")
+        pywhatkit.playonyt(command)
+
+    elif 'time' in command:
+        time = datetime.datetime.now().strftime('%I:%M %p')
+        speak("The current time is "+time)
+    
+    #Searching for someone on wikipedia
+
+    elif 'who is' in command:
+        command = command.replace('who is', '')
+        response = wikipedia.summary(command, 5)
+        print(response)
+        speak(response)
+
+    
+
+main()
+
+
+#https://www.lfd.uci.edu/~gohlke/pythonlibs
